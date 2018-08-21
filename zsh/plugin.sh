40e1412d2fce6ccd2a2fb985e8450dddf2a94522
Sagit <ourfor@foxmail.com>
2018-08-21 12:04:21 +0800

    Create plugin.sh

---
+++ zsh/plugin.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# author:ourfor date:20180821 website:ourfor.top
+# 一些zsh插件的安装
+
+#安装zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  #github仓库更新安装
