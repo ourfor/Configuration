17aac5c4a17dfabd99b1fa5e744a8ed6c01e350e
Sagit <ourfor@foxmail.com>
2018-08-21 11:29:52 +0800

    Update 解决外接硬盘电流不足.sh

--- raspberry/3/解决外接硬盘电流不足.sh
+++ raspberry/3/解决外接硬盘电流不足.sh
@@ -2,5 +2,5 @@
 #author:ourfor website:ourfor.top
 #解决外接机械硬盘供电不足。修改config.txt
 
-echo 'max_usb_current=1' >> /boot/EFI/config.txt
-echo 'safe_mode_gpio=4' >> /boot/EFI/config.txt
+sudo echo 'max_usb_current=1' >> /boot/efi/config.txt
+sudo echo 'safe_mode_gpio=4' >> /boot/efi/config.txt
