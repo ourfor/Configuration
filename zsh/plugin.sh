21bfdddf48b86bfbd2b81639ebbb39eee08ebea8
Sagit <ourfor@foxmail.com>
2018-09-09 17:52:41 +0800

    Update plugin.sh

--- zsh/plugin.sh
+++ zsh/plugin.sh
@@ -42,6 +42,6 @@ clear
 echo "Please change the directory to neofetch and install the package 'make', and then do the command \"make install\", finally do \"cd ~;source .zshrc\""
 
 echo "*****************************************"
-tips "Important: You may execute this script twice\nSupport: ourfor\nContect:\n- qq:2320713747\n- E-mail:ourfor@foxmail.com\nWebsite:https://ourfor.top"
+echo "Important: You may execute this script twice\nSupport: ourfor\nContect:\n- qq:2320713747\n- E-mail:ourfor@foxmail.com\nWebsite:https://ourfor.top"
 echo "*****************************************"
 
